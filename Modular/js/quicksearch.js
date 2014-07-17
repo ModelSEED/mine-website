9227687101d45eb39adee8a1e90f9a551aa7952c
James Jeffryes <jamesgjeffryes@gmail.com>
2014-07-16 20:45:38 -0600

    Now can use hit enter key to do quicksearch

--- Modular/js/quicksearch.js
+++ Modular/js/quicksearch.js
@@ -2,7 +2,7 @@ angular.module('app').factory('DbChoice', function(){
     return {dbid:'KEGGexp'};
 });
 
-angular.module('app').controller('mineCtl',  function ($scope,DbChoice) {
+angular.module('app').controller('mineCtl',  function ($scope,$state,DbChoice) {
     $scope.databases =  [
         {id:0, name:'KEGG',  db :'KEGGexp'},
         {id:1, name:'EcoCyc', db : 'EcoCycexp'},
@@ -11,5 +11,11 @@ angular.module('app').controller('mineCtl',  function ($scope,DbChoice) {
     $scope.database = $scope.databases[0]; 
     $scope.$watch('database', function() {
         DbChoice.dbid = $scope.database.db;
+
+    $scope.doQuickSearch = function(ev) {
+        if (ev.which==13){
+            $state.go("compounds",{search:$scope.name+','+$scope.database.db});
+        }
+    }
     });
 });

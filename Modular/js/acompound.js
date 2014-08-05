3ae1d0ae3e4a7b4f4673c5dd59bbfe9e4b18edbf
James Jeffryes <jamesgjeffryes@gmail.com>
2014-08-05 14:42:43 -0500

    fixed refresh issue with reactions

--- Modular/js/acompound.js
+++ Modular/js/acompound.js
@@ -1,11 +1,39 @@
 // Allows for communication between controlers
-angular.module('app').factory('CompoundDataFactory', function(){
-    return {compound:{}};
+angular.module('app').factory('CompoundDataFactory', function($rootScope){
+    var services = new mineDatabaseServices('http://bio-data-1.mcs.anl.gov/services/mine-database');
+    var out = {
+        getCompound: function (db, id){
+            var promise = services.get_comps(db, [id]);
+            promise.then(
+                function(result){
+                    out.compound = result[0];
+                    $rootScope.$broadcast("compoundLoaded")
+                },
+                function(err){
+                    console.log("get_comps fail");
+                })
+        }
+    };
+    return out
 });
 
 angular.module('app').controller('acompoundCtl', function($scope,$stateParams,DbChoice,CompoundDataFactory){
     var services = new mineDatabaseServices('http://bio-data-1.mcs.anl.gov/services/mine-database');
     var test_db = DbChoice.dbid;
+
+    if (!CompoundDataFactory.compound) {
+        CompoundDataFactory.getCompound(DbChoice.dbid, $stateParams.id);
+    }
+    else {
+        $scope.data = CompoundDataFactory.compound;
+    }
+    $scope.$on("compoundLoaded", function () {
+        console.log("compLoaded");
+        $scope.data = CompoundDataFactory.compound;
+        $scope.$apply();
+    });
+
+    /*
     $scope.search = $stateParams.id;
     $scope.data = CompoundDataFactory.compound;
     promise = services.get_comps(test_db, [$scope.search]);
@@ -21,7 +49,7 @@ angular.module('app').controller('acompoundCtl', function($scope,$stateParams,Db
             $scope.data =[];
             $scope.$apply();
         }
-    );
+    );*/
 
     $scope.launch_lightbox = function () {
         $("#cpd-img").lightbox_me({
@@ -72,27 +100,37 @@ angular.module('app').controller('productsCtl', function($scope,$stateParams,DbC
     $scope.items=0;
     $scope.numPages =0;
     $scope.searchOn = "";
-
-
     var services = new mineDatabaseServices('http://bio-data-1.mcs.anl.gov/services/mine-database');
-    var test_db = DbChoice.dbid;
-    var promise = services.get_rxns(test_db,CompoundDataFactory.compound.Product_of);
-    promise.then(function(result){
-            $scope.products = result;
-            console.log(result);
-            $scope.items = result.length
-            $scope.totalItems = $scope.products.length;
-            $scope.numPages = Math.ceil($scope.products.length / $scope.numPerPage)
-            $scope.$apply();
-        },
-        function(err){
-            console.log("productsCtl fail");
-            $scope.products =[];
-            $scope.$apply();
-        }
-    );
+    if (!CompoundDataFactory.compound) {
+        CompoundDataFactory.getCompound(DbChoice.dbid, $stateParams.id);
+    }
+    else {
+        get_products()
+    }
+    function get_products() {
+        var promise = services.get_rxns(DbChoice.dbid, CompoundDataFactory.compound.Product_of);
+        promise.then(function (result) {
+                $scope.products = result;
+                $scope.items = result.length
+                $scope.totalItems = $scope.products.length;
+                $scope.numPages = Math.ceil($scope.products.length / $scope.numPerPage)
+                $scope.$apply();
+            },
+            function (err) {
+                console.log("productsCtl fail");
+                $scope.products = [];
+                $scope.$apply();
+            }
+        );
+    }
+
+    $scope.$on("compoundLoaded", function () {
+        console.log("compLoaded");
+        get_products();
+    });
+
     $scope.getCompoundName= function(id){
-        var gPromise = services.get_comps(test_db, [id]);
+        var gPromise = services.get_comps(DbChoice.dbid, [id]);
         gPromise.then(
             function(result){
                 //console.log(result[0]);
@@ -119,14 +157,14 @@ angular.module('app').controller('productsCtl', function($scope,$stateParams,DbC
 
     $scope.$watch('currentPage + products + items + searchOn', function() {
         if((typeof($scope.products) != 'undefined') &&($scope.products.length > 0)){
-var subList = [];
+        var subList = [];
             for (var i = $scope.products.length - 1; i >= 0; i--) {
-                console.log($scope.products[i].Operators[0]);
+                //console.log($scope.products[i].Operators[0]);
                 if ($scope.products[i].Operators[0].indexOf($scope.searchOn) > -1){
                     subList.push($scope.products[i]);
                 }
             }
-            $scope.numPages = Math.ceil(subList.length / $scope.numPerPage)
+            $scope.numPages = Math.ceil(subList.length / $scope.numPerPage);
             var begin = (($scope.currentPage - 1) * $scope.numPerPage);
             var end = begin + $scope.numPerPage;
             $scope.filteredData = subList.slice(begin, end);        }
@@ -142,56 +180,72 @@ angular.module('app').controller('reactantsCtl', function($scope,$stateParams,Db
     $scope.numPages =0;
     $scope.searchOn = "";
     var services = new mineDatabaseServices('http://bio-data-1.mcs.anl.gov/services/mine-database');
-    var test_db = DbChoice.dbid;
-    promise = services.get_rxns(test_db,CompoundDataFactory.compound.Reactant_in);
-    promise.then(function(result){
-            $scope.reactants = result;
-            $scope.totalItems = $scope.reactants.length;
-            $scope.numPages = Math.ceil($scope.reactants.length / $scope.numPerPage)
-            $scope.$apply();
-        },
-        function(err){
-            console.log("reactantsCtl fail");
-            $scope.reactants =[];
-            $scope.$apply();
-        }
-    );
-    $scope.getCompoundName= function(id){
-      gPromise = services.get_comps(test_db, [id]);
-      gPromise.then(
-          function(result){
-              //console.log(result[0]);
-              if((id.substring(0, 1) == "X")||(result[0].Names==null)){
-                console.log("none"+id);
-                if(result[0].MINE_id==null){
-                  $scope.cName = "";
-                }else{
-                  $scope.cName = "Id:"+result[0].MINE_id;
-                }
-              }else{
-                console.log(result[0]);
-                $scope.cName = "Id:"+result[0].MINE_id+"\t"+"Name:"+result[0].Names[0];
-              }
-              $scope.$apply();
-          },
-          function(err){
-              console.log("acompoundCtl fail");
-              $scope.data =[];
-              $scope.$apply();
-          }
-      );
+
+    if (!CompoundDataFactory.compound){
+        CompoundDataFactory.getCompound(DbChoice.dbid, $stateParams.id);
     }
+    else{
+        get_reactants()
+    }
+
+    function get_reactants() {
+        var promise = services.get_rxns(DbChoice.dbid, CompoundDataFactory.compound.Reactant_in);
+        promise.then(function (result) {
+                $scope.reactants = result;
+                $scope.items = result.length;
+                $scope.totalItems = $scope.reactants.length;
+                $scope.numPages = Math.ceil($scope.reactants.length / $scope.numPerPage)
+                $scope.$apply();
+            },
+            function (err) {
+                console.log("reactantsCtl fail");
+                $scope.reactants = [];
+                $scope.$apply();
+            }
+        );
+    }
+
+    $scope.$on("compoundLoaded", function () {
+        console.log("compLoaded");
+        get_reactants();
+    });
+
+    $scope.getCompoundName= function(id){
+        var gPromise = services.get_comps(DbChoice.dbid, [id]);
+        gPromise.then(
+            function(result){
+                //console.log(result[0]);
+                if((id.substring(0, 1) == "X")||(result[0].Names==null)){
+                    console.log("none"+id);
+                    if(result[0].MINE_id==null){
+                        $scope.cName = "";
+                    }else{
+                        $scope.cName = "Id:"+result[0].MINE_id;
+                    }
+                }else{
+                    console.log(result[0]);
+                    $scope.cName = "Id:"+result[0].MINE_id+"\t"+"Name:"+result[0].Names[0];
+                }
+                $scope.$apply();
+            },
+            function(err){
+                console.log("acompoundCtl fail");
+                $scope.data =[];
+                $scope.$apply();
+            }
+        );
+    };
 
     $scope.$watch('currentPage + reactants + items+searchOn', function() {
         if((typeof($scope.reactants) != 'undefined') &&($scope.reactants.length > 0)){
             var subList = [];
             for (var i = $scope.reactants.length - 1; i >= 0; i--) {
-                console.log($scope.reactants[i].Operators[0]);
+                //console.log($scope.reactants[i].Operators[0]);
                 if ($scope.reactants[i].Operators[0].indexOf($scope.searchOn) > -1){
                     subList.push($scope.reactants[i]);
                 }
-            };
-            $scope.numPages = Math.ceil(subList.length / $scope.numPerPage)
+            }
+            $scope.numPages = Math.ceil(subList.length / $scope.numPerPage);
             var begin = (($scope.currentPage - 1) * $scope.numPerPage);
             var end = begin + $scope.numPerPage;
             $scope.filteredData = subList.slice(begin, end);

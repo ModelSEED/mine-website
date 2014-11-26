616daa1f8a6291f5aca9b730592e6f2130c320d2
James Jeffryes <jamesgjeffryes@gmail.com>
2014-11-26 12:22:03 -0600

    added in NOT functionality

--- Modular/js/textSearch.js
+++ Modular/js/textSearch.js
@@ -7,9 +7,9 @@ angular.module('app').controller('quickSearchCtl',  function ($scope,$state) {
 angular.module('app').controller('advancedSearchCtl', function($scope,$state){
     $scope.and = [];
     $scope.or = [];
-    $scope.not = [];
     $scope.value = "";
     $scope.RegEx = false;
+    $scope.not = false;
     $scope.fields = [
         {'id': 0, 'name': '"_id"'}, {'id': 1, 'name': '"SMILES"'}, {'id': 2, 'name': '"NP_likeness"'},
         {'id': 3, 'name': '"MINE_id"'}, {'id': 4, 'name': '"Inchikey"'}, {'id': 5, 'name': '"Charge"'},
@@ -24,20 +24,22 @@ angular.module('app').controller('advancedSearchCtl', function($scope,$state){
     $scope.selected = $scope.fields[0];
     $scope.addItem = function(array, field, value){
         if (value) {
-            if (parseFloat(value+1)) array.push([field, value, $scope.RegEx]);
-            else array.push([field, '"'+value+'"', $scope.RegEx]);
+            if (parseFloat(value+1)) array.push([field, value, $scope.RegEx, $scope.not]);
+            else array.push([field, '"'+value+'"', $scope.RegEx, $scope.not]);
         }
     };
 
     $scope.removeRow = function(array, index){
         array.splice(index, 1);
     };
+
     var mongoizeArray = function(query, operator, array){
         if (query) query += ", ";
         else query += "{";
         query += operator+":[";
         for (var i = 0; i < array.length; i++) {
             if (array[i][2]) query += "{" + array[i][0] + ' : {"$regex":' + array[i][1] + "}}, ";
+            else if (array[i][3]) query += "{" + array[i][0] + ' : {"$ne":' + array[i][1] + "}}, ";
             else query += "{" + array[i][0] + ":" + array[i][1] + "}, ";
         }
         query = query.substring(0,query.length-2) + "]";
@@ -45,11 +47,10 @@ angular.module('app').controller('advancedSearchCtl', function($scope,$state){
 
     };
 
-    $scope.search = function(and,or,not){
+    $scope.search = function(and,or){
         var query = "";
         if (and.length) query = mongoizeArray(query, '"$and"', and);
         if (or.length) query = mongoizeArray(query, '"$or"', or);
-        //if (not.length) query = mongoizeArray(query, '"$not"', not);
         query += "}";
         console.log(query);
         $state.go('compounds', {'search':query})
